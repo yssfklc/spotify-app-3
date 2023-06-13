0aa161a532595a8e4c316125af2bfc4dcb2b3f12
Yusuf Kılıç <y.kilic@superpay.tech>
2023-06-13 20:00:46 +0300

    changes one june of 13th

--- src/App.js
+++ src/App.js
@@ -11,100 +11,72 @@ function App() {
   // post();
 var client_id = 'b4c2a14fb8734fff86e81c01ff04ab40';
 var client_secret = 'a3e2b55d47854f0582e8c767b2bf04e1';
-var access_token='BQDKBhQFyJ6GT--aYfBAqYPhCh2aIF53phrcBVIKGtN4lHXxbjh9U9MZZP7pg3S5G4Vc5EwLI3gaLjOgZD2lhcNBRalJ0UJdjQBDMoeltuXnHkkiPMc';
-const [isTrue, setIsTrue] = useState(false);
-const [listfortrack, setTrack] = useState();
-const [data, setData]=useState({})
-const n=[];
-const i=[];
+const [access_token, setAccess_token]=useState('');
+const [value, setValue]=useState('');
 
 
-
-// async function post(){
-//   const jsonData=await fetch("https://accounts.spotify.com/api/token",{
-//     method:"POST",
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//       'Authorization': 'Basic ' + 'YjRjMmExNGZiODczNGZmZjg2ZTgxYzAxZmYwNGFiNDA6YTNlMmI1NWQ0Nzg1NGYwNTgyZThjNzY3YjJiZjA0ZTE='
-//     },
-//     body:'grant_type=client_credentials'
+useEffect(()=>{
+  async function post(){
+    const jsonData=await fetch("https://accounts.spotify.com/api/token",{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + 'YjRjMmExNGZiODczNGZmZjg2ZTgxYzAxZmYwNGFiNDA6YTNlMmI1NWQ0Nzg1NGYwNTgyZThjNzY3YjJiZjA0ZTE='
+      },
+      body:'grant_type=client_credentials'
+        
       
     
+    })
+    const data=await jsonData.json();
+    setAccess_token(data.access_token);
+    
+  };
+  post();
   
-//   })
-//   const data=await jsonData.json();
-  
-//   return data.access_token;
-  
-// };
-
-// console.log(post());
+}, [])
 
 
-// get artist
-async function getArtist(){
-  const data=await fetch('https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb',{
-    headers: {Authorization : `Bearer ${access_token}`}
-  })
-
-  const json=await data.json();
-  
-  return json;
-}
 
 
-  // async function getSearch(){
-  //   try {
-  //     const q='remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis';
-  //     const type='type=artist';
-  //     const response = await fetch(('https://api.spotify.com/v1/search?q=' + q + '&' + type),{ 
-  //     headers: {Authorization : `Bearer ${access_token}`}
-      
-  //     }
-     
-  //   )
-  //   if(response.ok){
-  //     const jsonData = await response.json();
-      
-  //     const id= jsonData.artists.items[0].id;
-  //     for( const item of jsonData.artists.items){
-  //       const secondResponse=await fetch(('https://api.spotify.com/v1/artists/' + item.id),{
-  //       headers: {Authorization : `Bearer ${access_token}`}
 
-  //   })
-  //   if(secondResponse.ok){ 
-  //     const secondJsonData = await secondResponse.json();
-  //     n.push(secondJsonData.name);
-  //     i.push(secondJsonData.images[0].url);  
-  //   }
-  //     }
-  //   }   
-  //   }catch(err){
-  //     console.log(err)
-  //   }
-  // };
-  
-  
 
-  setData(getArtist());
-  console.log(data)
-  // console.log(n);
-  // console.log(i);
+  const handleClick=()=>{
+    async function getSearchRequest(){
+      const jsonData = await fetch('https://api.spotify.com/v1/search?q='+ value +'&type=album' , {
+        method:'GET',
+        headers:{Authorization: `Bearer ${access_token}`}
+      })
+      const data=await jsonData.json();
+      console.log(data)
+    }
+    getSearchRequest();
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-
-        <SearchBar  />
-        <div className='App-body' >
-          <div className='playlist-container'>
-              <div className='playlist-body'>
-                <PlayList isTrue={isTrue} setIsTrue={setIsTrue} names={n} img={i} setTrack={setTrack} />
-              </div>
+      <div className='search-container'>
+        <form>
+          <input onChange={e=>setValue(e.target.value)} />
+          <button onClick={handleClick()} type='submit' >Search</button>
+        </form>
+      </div>
+      <div className='list-container'>
+        <div className='default-list'>
+          <div className='card'>
+            <h2>test1</h2>
+            <p>test1</p>
+            <img/>
           </div>
-          <TrackList isTrue={isTrue} setIsTrue={setIsTrue} listfortrack={listfortrack} />
-      
         </div>
-      </header>
+        <div className='my-list'>
+        <div className='card'>
+            <h2>test1</h2>
+            <p>test1</p>
+            <img/>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
